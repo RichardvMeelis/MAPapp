bfc9ff9843c35d8d046a3feec4103b841bd2518f
Arjan <arjan154321@live.nl>
2017-01-18 15:01:58 +0100

    commit

--- MAPapp/MAPapp/MAPapp/Pages/AccountSettings.cs
+++ MAPapp/MAPapp/MAPapp/Pages/AccountSettings.cs
@@ -9,16 +9,65 @@ namespace MAPapp
 {
 	public class AccountSettings : ContentPage
 	{
-		public AccountSettings ()
+            Label bedrijflabel, naamlabel, achternaamlabel, emaillabel, naamlabel1, bedrijflabel1, achternaamlabel1, emaillabel1;
+            ClickableLabel veranderWachtwoord, veranderEmail;
+        public AccountSettings()
 
-		{
-			Content = new StackLayout { 
-                Margin = GeneralSettings.pageMargin,
-				Children = {
-					new Label { Text = "Wachtwoord" }
-                    
-				}
-			};
-		}
-	}
+        {
+                // moet mogelijk in een andere pagina komen te staan zodat meer pagina's met de variabele kunnen werken
+                GetFromDatabase.currentUser =  (User)GetFromDatabase.GetUserInfo(GetFromDatabase.currentUserName, GetFromDatabase.currentToken); 
+                
+                // het creëren van een Grid
+                var grid1 = new Grid { RowSpacing = 1, ColumnSpacing = 1 };
+
+                // 8 rijen toevoegen aan de grid
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+                // 3 colums toevoegen aan de grid
+                grid1.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                grid1.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                grid1.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+                // labels toevoegen aan pleken in de grid
+                grid1.Children.Add(naamlabel = new Label() { Text = "Naam: ", FontSize = 20 }, 0, 0);
+                grid1.Children.Add(achternaamlabel = new Label() { Text = "Achternaam: ", FontSize = 20 }, 0, 1);
+                grid1.Children.Add(bedrijflabel = new Label() { Text = "Bedrijf: ", FontSize = 20 }, 0, 2);
+                grid1.Children.Add(emaillabel = new Label() { Text = "Email:", FontSize = 20 }, 0, 3);
+                grid1.Children.Add(naamlabel1 = new Label() { Text = GetFromDatabase.currentUser.firstname, FontSize = 20 }, 1, 0);
+                grid1.Children.Add(achternaamlabel1 = new Label() { Text = GetFromDatabase.currentUser.lastname, FontSize = 20 }, 1, 1);
+                grid1.Children.Add(bedrijflabel1 = new Label() { Text = GetFromDatabase.currentUser.companyname, FontSize = 20 }, 1, 2);
+                grid1.Children.Add(emaillabel1 = new Label() { Text = GetFromDatabase.currentUser.username, FontSize = 20 }, 1, 3);
+                grid1.Children.Add(veranderWachtwoord = new ClickableLabel(veranderwachtwoord) { Text = "verander wachtwoord", TextColor = Color.Aqua }, 0, 4);
+                grid1.Children.Add(veranderEmail = new ClickableLabel(veranderemail) { Text = "verander email", TextColor = Color.Aqua, }, 0, 5);
+
+                // laat de tweede colom twee keer zo groot zijn als de eerste
+                Grid.SetColumnSpan(naamlabel1, 2);
+                Grid.SetColumnSpan(emaillabel1, 2);
+                Grid.SetColumnSpan(bedrijflabel1, 2);
+                Grid.SetColumnSpan(veranderWachtwoord, 2);
+                Grid.SetColumnSpan(veranderEmail, 2);
+
+                Content = grid1;
+            }
+
+            // ga naar NewEmail page
+            public async void veranderemail(string s)
+            {
+                await Navigation.PushAsync(new NewEmail());
+            }
+            
+            // ga naar NewWachtwoord page
+            public async void veranderwachtwoord(string s)
+            {
+                await Navigation.PushAsync(new NewWachtwoord());
+            }
+        }
 }
---
+++ MAPapp/MAPapp/MAPapp/Pages/NewWachtwoord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MAPapp
+{
+	public class NewWachtwoord : ContentPage
+	{
+        Entry oudwachtwoord = new Entry() { Placeholder = "huidig wachtwoord", IsPassword= true }, nieuwWachtwoord= new Entry() {Placeholder="nieuw wachtwoord", IsPassword=true }, nieuwwachtwoord2 = new Entry() {Placeholder="herhaal nieuw wachtwoord", IsPassword= true } ;
+        Button change = new Button() { Text = "verander", IsEnabled = false };
+
+        public NewWachtwoord ()
+		{
+
+            oudwachtwoord.TextChanged += textverandert;
+            nieuwWachtwoord.TextChanged += textverandert;
+            nieuwwachtwoord2.TextChanged += textverandert;
+
+            Content = new StackLayout
+            {
+                Children = { new Label { Text = "huidig wachtwoord:", FontSize=15 }, oudwachtwoord, new Label() {Text="nieuw wachtwoord:"}, nieuwWachtwoord, new Label() {Text="herhaal nieuw wachtwoord" }, nieuwwachtwoord2, change }
+			};
+
+		}
+
+        private void textverandert(object sender, TextChangedEventArgs e)
+        {
+            if (oudwachtwoord.Text != null && nieuwWachtwoord.Text != null && nieuwwachtwoord2 != null && nieuwWachtwoord.Text.Length >= 6 && nieuwWachtwoord.Text == nieuwwachtwoord2.Text) change.IsEnabled = true;
+            else change.IsEnabled = false;
+        }
+	}
+}
